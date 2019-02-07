6517ad5ac5b1e852bd019bfa3c6e3a981fa186b5
langongjin <lanmessi@hotmail.com>
2019-02-07 15:07:47 +0800

    Add files via upload

--- run-object.py
+++ run-object.py
@@ -1,6 +1,6 @@
-import objectNEAT_time
+import objectNEAT_2classRGB
 
-save_path = './paper4/'
+save_path = './train2000-2/'
 number_of_generations = 2001
 testset_size = 0.25
-objectNEAT_time.run_objectrecognition(number_of_generations, save_path, testset_size)
+objectNEAT_2classRGB.run_objectrecognition(number_of_generations, save_path, testset_size)
